cb5ef3c39fc88db11a673bfbeab351c67de4d299
Sally Kingston <sally.a.kingston@gmail.com>
2015-10-22 20:20:14 -0400

    finished normal mode except styling

---
+++ main.js
@@ -0,0 +1,75 @@
+
+//default: show first picture from each album, with album labels
+var imgTemplate = _.template($("#imgTmpl").html());
+var buttonTemplate = _.template($("#albumBtnTmpl").html());
+var mainText = "";
+var buttonText = "";
+
+$(document).ready(function(){
+  _.each(albums,function (album) {
+    mainText = imgTemplate(album.photos[0]);
+    buttonText = buttonTemplate(album);
+    $(".albums").append(mainText);
+    $("aside").append(buttonText);
+  });
+  $("aside").addClass("hidden");
+  bindButtonClickEvent();
+  bindAlbumClickEvent();
+});
+
+//when you click an album cover, hide the albums and show the pictures in that album
+var bindAlbumClickEvent = function () {
+  $(".albums img").on("click",function (event) {
+    event.preventDefault();
+    $(".albums").addClass("hidden");
+    $("aside").removeClass("hidden");
+    $(".photos").removeClass("hidden");
+    $(".photos").html("");
+    var displayedAlbumName = $(this).attr('src').split("/")[1];
+    displayAlbum(displayedAlbumName);
+  });
+};
+
+var bindButtonClickEvent = function() {
+  $("button").on("click",function (event) {
+    event.preventDefault();
+    $(".photos").html("");
+    var displayedAlbumName = $(this).text();
+    if (displayedAlbumName.split(" ").length>1){
+      displayedAlbumName = displayedAlbumName.split(" ")[2];
+    }
+    displayAlbum(displayedAlbumName);
+  });
+};
+
+var bindPhotoClickEvent = function(){
+  $(".photos img").on("click",function(event) {
+    event.preventDefault();
+    displayPhoto($(this));
+  });
+};
+
+var displayAlbum = function(albumName){
+  var displayedAlbum = _.filter(albums,function (album) {
+    return (album.albumName === albumName);
+  })[0];
+  _.each(displayedAlbum.photos, function (photo) {
+    mainText = imgTemplate(photo);
+    $(".photos").append(mainText);
+  });
+  bindPhotoClickEvent();
+  buttonText = buttonTemplate(displayedAlbum);
+  $(".photos").removeClass("hidden");
+  $("aside").removeClass("hidden");
+  $(".photoDetails").addClass("hidden");
+  $(".photoDetails").html(buttonText);
+  $(".photoDetails button").prepend("Back to ");
+  bindButtonClickEvent();
+};
+
+var displayPhoto = function (photoText) {
+  $(".photos").addClass("hidden");
+  $(".photoDetails").removeClass("hidden");
+  $(".photoDetails").append(photoText);
+  $("aside").addClass("hidden");
+};
---
+++ photos.js
@@ -0,0 +1,130 @@
+var albums = [{
+    albumName: "cristacarmen",
+    photos: [{
+        imgURL: "cristacarmen/photo1.jpg",
+        photoTitle: "Crista"
+    }, {
+        imgURL: "cristacarmen/photo2.jpg",
+        photoTitle: "Christmas"
+    }, {
+        imgURL: "cristacarmen/photo3.jpg",
+        photoTitle: "Children Yelling"
+    }, {
+        imgURL: "cristacarmen/photo4.jpg",
+        photoTitle: "Children Smiling"
+    }, {
+        imgURL: "cristacarmen/photo5.jpg",
+        photoTitle: "Hug"
+    }, {
+        imgURL: "cristacarmen/photo6.jpg",
+        photoTitle: "Girls"
+    }, {
+        imgURL: "cristacarmen/photo7.jpg",
+        photoTitle: "Carmen"
+    }, {
+        imgURL: "cristacarmen/photo8.jpg",
+        photoTitle: "Carmen"
+    }, {
+        imgURL: "cristacarmen/photo9.jpg",
+        photoTitle: "Crista"
+    }]
+}, {
+    albumName: "halloween",
+    photos: [{
+        imgURL: "halloween/photo1.jpg",
+        photoTitle: "Halloween"
+    }, {
+        imgURL: "halloween/photo2.jpg",
+        photoTitle: "Halloween"
+    }, {
+        imgURL: "halloween/photo3.jpg",
+        photoTitle: "Halloween"
+    }, {
+        imgURL: "halloween/photo4.jpg",
+        photoTitle: "Halloween"
+    }, {
+        imgURL: "halloween/photo5.jpg",
+        photoTitle: "Halloween"
+    }, {
+        imgURL: "halloween/photo6.jpg",
+        photoTitle: "Halloween"
+    }]
+}, {
+    albumName: "rebecca",
+    photos: [{
+        imgURL: "rebecca/photo1.jpg",
+        photoTitle: "Baby Becca"
+    }, {
+        imgURL: "rebecca/photo2.jpg",
+        photoTitle: "Baby Becca"
+    }, {
+        imgURL: "rebecca/photo3.jpg",
+        photoTitle: "Baby Becca"
+    }, {
+        imgURL: "rebecca/photo4.jpg",
+        photoTitle: "Baby Becca"
+    }, {
+        imgURL: "rebecca/photo5.jpg",
+        photoTitle: "Baby Becca"
+    }, {
+        imgURL: "rebecca/photo6.jpg",
+        photoTitle: "Baby Becca"
+    }, {
+        imgURL: "rebecca/photo7.jpg",
+        photoTitle: "Baby Becca"
+    }]
+}, {
+    albumName: "springBreak",
+    photos: [{
+        imgURL: "springBreak/photo1.jpg",
+        photoTitle: "Cousins"
+    }, {
+        imgURL: "springBreak/photo2.jpg",
+        photoTitle: "Cousins"
+    }, {
+        imgURL: "springBreak/photo3.jpg",
+        photoTitle: "Cousins"
+    }]
+}, {
+    albumName: "thanksgiving",
+    photos: [{
+        imgURL: "thanksgiving/photo1.jpg",
+        photoTitle: "Leaves"
+    }, {
+        imgURL: "thanksgiving/photo2.jpg",
+        photoTitle: "Leaves"
+    }, {
+        imgURL: "thanksgiving/photo3.jpg",
+        photoTitle: "Leaves"
+    }, {
+        imgURL: "thanksgiving/photo4.jpg",
+        photoTitle: "Leaves"
+    }, {
+        imgURL: "thanksgiving/photo5.jpg",
+        photoTitle: "Leaves"
+    }]
+}, {
+    albumName: "the80s",
+    photos: [{
+        imgURL: "the80s/photo1.jpg",
+        photoTitle: "Kids"
+    }, {
+        imgURL: "the80s/photo2.jpg",
+        photoTitle: "Kids"
+    }, {
+        imgURL: "the80s/photo3.jpg",
+        photoTitle: "Kids"
+    }, {
+        imgURL: "the80s/photo4.jpg",
+        photoTitle: "Kids"
+    }, {
+        imgURL: "the80s/photo5.jpg",
+        photoTitle: "Kids"
+    }, {
+        imgURL: "the80s/photo6.jpg",
+        photoTitle: "Kids"
+    }, {
+        imgURL: "the80s/photo7.jpg",
+        photoTitle: "Kids"
+    }]
+}];
