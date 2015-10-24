4f769b749dbef4b5655c19319b2d3eb440fe312a
Sally Kingston <sally.a.kingston@gmail.com>
2015-10-24 11:13:56 -0400

    finished hard mode

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 //default: show first picture from each album, with album labels
 var imgTemplate = _.template($("#imgTmpl").html());
 var buttonTemplate = _.template($("#albumBtnTmpl").html());
+var bigImgTemplate = _.template($("#bigImgTmpl").html());
 var mainText = "";
 var buttonText = "";
 
@@ -13,19 +14,14 @@ $(document).ready(function(){
 var bindAlbumClickEvent = function () {
   $(".albums img").on("click",function (event) {
     event.preventDefault();
-    $(".albums").addClass("hidden");
-    $("aside").removeClass("hidden");
-    $(".photos").removeClass("hidden");
-    $(".photos").html("");
     var displayedAlbumName = $(this).attr('src').split("/")[1];
     displayAlbum(displayedAlbumName);
   });
 };
 
 var bindButtonClickEvent = function() {
-  $("button").on("click",function (event) {
+  $(".albumButton").on("click",function (event) {
     event.preventDefault();
-    $(".photos").html("");
     var displayedAlbumName = $(this).attr("rel");
     displayAlbum(displayedAlbumName);
   });
@@ -34,11 +30,23 @@ var bindButtonClickEvent = function() {
 var bindPhotoClickEvent = function(){
   $(".photos img").on("click",function(event) {
     event.preventDefault();
-    displayPhoto($(this).parent());
+    var displayedAlbum = _.find(albums,function (album) {
+      return (album.albumTitle === $(".pageTitle").text());
+    });
+    var photoIndex = $(".photos").children().index($(this).parent().parent());
+    // displayPhoto($(this).parent());
+    displayPhoto(displayedAlbum,photoIndex);
+    $(".pageTitle").text($(this).attr("alt"));
+    $("header").css({
+      "background-color": "darkgray",
+      "width":"100%"
+    });
+    $("body").css("background-color","darkgray");
   });
 };
 
 var displayAlbumCovers = function(){
+    $("aside").html("");
     _.each(albums,function (album) {
       mainText = imgTemplate(album.photos[0]);
       buttonText = buttonTemplate(album);
@@ -47,32 +55,81 @@ var displayAlbumCovers = function(){
       $( "p:last" )[0].textContent=album.albumTitle;
       $("aside").append(buttonText);
     });
+    $("aside").append("<button class = 'returnHome'>Home</button>");
+    $(".returnHome").on("click",function (event) {
+      event.preventDefault();
+      displayAlbumCovers();
+      $("body").css("background-color","white");
+      $("header").css({
+        "color": "white",
+        "width": "100%",
+        "background-color": "black",
+        "float": "none",
+        "margin-top": "0",
+        "padding-bottom": "30px"
+      });
+      $(".pageTitle").text("My Albums");
+    });
     $("aside").addClass("hidden");
+    $(".albums").removeClass("hidden");
     bindButtonClickEvent();
     bindAlbumClickEvent();
 };
 
 var displayAlbum = function(albumName){
-  var displayedAlbum = _.filter(albums,function (album) {
+  $(".photos").html("");
+  var displayedAlbum = _.find(albums,function (album) {
     return (album.albumName === albumName);
-  })[0];
+  });
   _.each(displayedAlbum.photos, function (photo) {
     mainText = imgTemplate(photo);
     $(".photos").append(mainText);
   });
   bindPhotoClickEvent();
-  buttonText = buttonTemplate(displayedAlbum);
+  // buttonText = buttonTemplate(displayedAlbum);
+  $(".albums").addClass("hidden");
+  $(".photoDetails").addClass("hidden");
   $(".photos").removeClass("hidden");
   $("aside").removeClass("hidden");
-  $(".photoDetails").addClass("hidden");
-  $(".photoDetails").html(buttonText);
-  $(".photoDetails button").prepend("Back to ");
-  bindButtonClickEvent();
+  // $(".photoDetails").html(buttonText);
+  // $(".photoDetails button").prepend(" < Back to ");
+  // bindButtonClickEvent();
+  $(".pageTitle").text(displayedAlbum.albumTitle);
+  $("body").css("background-color","black");
+  $("header").css({
+    "color": "black",
+    "width": "85%",
+    "background-color": "white",
+    "float": "right",
+    "margin-top": "0",
+    "padding-top": "30px"
+  });
 };
 
-var displayPhoto = function (photoText) {
+var displayPhoto = function (displayedAlbum,photoIndex) {
+  $(".photoDetails").html("");
+  buttonText = buttonTemplate(displayedAlbum);
+  $(".photoDetails").html(buttonText);
+  $(".photoDetails button").prepend(" < Back to ");
+  bindButtonClickEvent();
   $(".photos").addClass("hidden");
   $(".photoDetails").removeClass("hidden");
-  $(".photoDetails").append(photoText);
+  $(".photoDetails").append(bigImgTemplate(displayedAlbum.photos[photoIndex]));
   $("aside").addClass("hidden");
+  $(".photoDetails").prepend("<div class='rightnav'>></div>");
+  $(".photoDetails").prepend("<div class='leftnav'><</div>");
+  $(".rightnav").on("click",function(){
+    if (photoIndex===displayedAlbum.photos.length-1){
+      displayPhoto(displayedAlbum,0);
+    }else{
+      displayPhoto(displayedAlbum,photoIndex+1);
+    }
+  });
+  $(".leftnav").on("click",function(){
+    if (photoIndex===0){
+      displayPhoto(displayedAlbum,displayedAlbum.photos.length-1);
+    }else{
+      displayPhoto(displayedAlbum,photoIndex-1);
+    }
+  });
 };
